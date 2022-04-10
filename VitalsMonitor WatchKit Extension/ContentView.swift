1c07ca0e41b5f0fc163877a6f1407d73660afa3e
JamesFoley1 <foley.r.james@gmail.com>
2022-04-10 11:27:18 -0700

    first commit

--- VitalsMonitor WatchKit Extension/ContentView.swift
+++ VitalsMonitor WatchKit Extension/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var secondScreenShown = false
+    @State var timerVal = 60
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack{
+            
+            Text("Start Timer for \(timerVal) seconds")
+            Picker(selection: $timerVal, label: Text("")) {
+                Text("60").tag(60)
+                Text("50").tag(50)
+                Text("40").tag(40)
+                Text("30").tag(30)
+                Text("20").tag(20)
+                Text("10").tag(10)
+            }
+            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: { Text("Go")})
+        }
+        
+    }
+}
+
+struct SecondView: View {
+    @Binding var secondScreenShown: Bool
+    @State var timerVal: Int
+    var body: some View {
+        VStack{
+            if timerVal > 0 {
+                Text("Emergency?").font(.system(size: 14))
+                Text("\(timerVal)").font(.system(size: 40))
+                    .onAppear(){
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+                            _ in if self.timerVal > 0 {
+                                self.timerVal -= 1
+                            }
+                        }
+                    }
+                Text("Please Respond").font(.system(size: 14))
+                
+                HStack{
+                    Button(action: {
+                        self.secondScreenShown = false
+                        NSLog("send alert to api")
+                    }) {
+                        Text("Yes").foregroundColor(.red)
+                    }
+                    .tint(.red)
+                    
+                    
+                    Button(action: {
+                        self.secondScreenShown = false
+                        NSLog("Go back to monitoring")
+//                        Consider a using a minimum / maximum time to monitor for emergency...
+                    }) {
+                        Text("No").foregroundColor(.green)
+                    }
+                    .tint(.green)
+                }
+                
+                
+            } else {
+                Button(action: {
+                    self.secondScreenShown = false
+                }) {
+                    Text("Done").foregroundColor(.green)
+                }
+                .tint(.green)
+            }
+         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+            ContentView()
+        }
     }
 }

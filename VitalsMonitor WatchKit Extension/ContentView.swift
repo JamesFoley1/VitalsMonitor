1f6f838b58865d4dc380cc57d3a7bf8b82d10f79
JamesFoley1 <foley.r.james@gmail.com>
2022-04-10 12:13:54 -0700

    stuff

--- VitalsMonitor WatchKit Extension/ContentView.swift
+++ VitalsMonitor WatchKit Extension/ContentView.swift
@@ -13,26 +13,15 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            
-            Text("Start Timer for \(timerVal) seconds")
-            Picker(selection: $timerVal, label: Text("")) {
-                Text("60").tag(60)
-                Text("50").tag(50)
-                Text("40").tag(40)
-                Text("30").tag(30)
-                Text("20").tag(20)
-                Text("10").tag(10)
-            }
-            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: { Text("Go")})
+                Text("Emergency?").font(.system(size: 14))
         }
-        
     }
 }
 
-struct SecondView: View {
+struct EmergencyResponse: View {
     @Binding var secondScreenShown: Bool
     @State var timerVal: Int
-    var body: some View {
+    var body : some View {
         VStack{
             if timerVal > 0 {
                 Text("Emergency?").font(.system(size: 14))
@@ -59,7 +48,7 @@ struct SecondView: View {
                     Button(action: {
                         self.secondScreenShown = false
                         NSLog("Go back to monitoring")
-//                        Consider a using a minimum / maximum time to monitor for emergency...
+    //                        Consider a using a minimum / maximum time to monitor for emergency...
                     }) {
                         Text("No").foregroundColor(.green)
                     }
@@ -75,9 +64,31 @@ struct SecondView: View {
                 }
                 .tint(.green)
             }
-         }
-    }
+        }
+     }
 }
+
+//struct SecondView: View {
+//    @Binding var secondScreenShown: Bool
+//    @State var timerVal: Int
+//    var body: some View {
+//        VStack{
+//
+//
+//
+//            Text("Start Timer for \(timerVal) seconds")
+//            Picker(selection: $timerVal, label: Text("")) {
+//                Text("60").tag(60)
+//                Text("50").tag(50)
+//                Text("40").tag(40)
+//                Text("30").tag(30)
+//                Text("20").tag(20)
+//                Text("10").tag(10)
+//            }
+//            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: { Text("Go")})
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

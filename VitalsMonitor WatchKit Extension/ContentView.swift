5f6bf3ac774b902c4dace8158e14d0a7e7df57ce
danielhmendoza01 <danielhmendoza01@gmail.com>
2022-04-11 19:32:26 -0700

    View Update

--- VitalsMonitor WatchKit Extension/ContentView.swift
+++ VitalsMonitor WatchKit Extension/ContentView.swift
@@ -1,99 +1,79 @@
 //
 //  ContentView.swift
-//  VitalsMonitor WatchKit Extension
+//  VitalTest WatchKit Extension
 //
-//  Created by James Foley on 4/9/22.
+//  Created by Daniel Mendoza on 4/11/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @State var secondScreenShown = false
-    @State var timerVal = 60
-    
+    @State var emergencyScreenShown = false
+    @State var timerVal = -1
+    @State var timerScreenShown = false
+    @State var heartRateFlag = false
     var body: some View {
         VStack{
-            Text("We are monitoring your health!").font(.system(size: 14)).foregroundColor(.green)
+            Text("We are monitoring your health!").foregroundColor(.green)
+                .padding()
+            NavigationLink(destination: emergencyScreen(emergencyScreenShown: $emergencyScreenShown), isActive: $emergencyScreenShown, label: {Text("Tap if emergency!").foregroundColor(.red)})
+            NavigationLink(destination: timerScreen(timerScreenShown: $timerScreenShown, emergencyScreenShown: $emergencyScreenShown), isActive: $timerScreenShown, label: {Text("Temp to Timer")})
         }
     }
 }
 
-struct EmergencyResponse: View {
-    @Binding var secondScreenShown: Bool
-    @State var timerVal: Int
-    var body : some View {
+struct emergencyScreen: View{
+    @Binding var emergencyScreenShown:Bool
+    var body: some View{
+        Text("Message has been sent to caretaker!").foregroundColor(.yellow).padding()
+    }
+}
+
+struct timerScreen: View{
+    @Binding var timerScreenShown:Bool
+    @Binding var emergencyScreenShown:Bool
+    //@state var heartRateFlag = true
+    @State var timerVal = 5
+    var body: some View {
         VStack{
             if timerVal > 0 {
                 Text("Emergency?").font(.system(size: 14))
-                Text("\(timerVal)").font(.system(size: 40))
-                    .onAppear(){
-                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
-                            _ in if self.timerVal > 0 {
-                                self.timerVal -= 1
-                            }
+                Text("\(timerVal)").font(.system(size: 40)).onAppear(){
+                    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){
+                        timer in if self.timerVal > 0 {
+                            self.timerVal -= 1
                         }
                     }
+                }
                 Text("Please Respond").font(.system(size: 14))
                 
                 HStack{
-                    Button(action: {
-                        self.secondScreenShown = false
-                        NSLog("send alert to api")
-                    }) {
-                        Text("Yes").foregroundColor(.red)
-                    }
+                    //Yes button
+                    NavigationLink(destination: emergencyScreen(emergencyScreenShown: $emergencyScreenShown), isActive: $emergencyScreenShown, label: {Text("Yes").foregroundColor(.red)})
                     .tint(.red)
                     
-                    
-                    Button(action: {
-                        self.secondScreenShown = false
-                        NSLog("Go back to monitoring")
-    //                        Consider a using a minimum / maximum time to monitor for emergency...
-                    }) {
-                        Text("No").foregroundColor(.green)
-                    }
+                    //No button
+                    NavigationLink(destination: ContentView(),label: {Text("No").foregroundColor(.green)})
                     .tint(.green)
+                    
+                    
+                    
                 }
-                
-                
-            } else {
-                Button(action: {
-                    self.secondScreenShown = false
-                }) {
-                    Text("Done").foregroundColor(.green)
-                }
-                .tint(.green)
             }
-        }
-     }
-}
-
-//struct SecondView: View {
-//    @Binding var secondScreenShown: Bool
-//    @State var timerVal: Int
-//    var body: some View {
-//        VStack{
-//
-//
-//
-//            Text("Start Timer for \(timerVal) seconds")
-//            Picker(selection: $timerVal, label: Text("")) {
-//                Text("60").tag(60)
-//                Text("50").tag(50)
-//                Text("40").tag(40)
-//                Text("30").tag(30)
-//                Text("20").tag(20)
-//                Text("10").tag(10)
-//            }
-//            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: { Text("Go")})
-//        }
-//    }
-//}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ContentView()
+            else if(timerVal==0){
+                emergencyScreen(emergencyScreenShown: $emergencyScreenShown)
+            }
+            else{
+                ContentView()
+            }
         }
     }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
